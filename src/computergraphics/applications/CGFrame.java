6b1eeaaa0d4ec5f364bce93414276eb14cc7cfff
Christian Schirin <Christian.Schirin@haw-hamburg.de>
2014-10-02 18:42:01 +0200

    Aufgabe 1a bearbeitet (Dreieck ist jetzt grün)

--- src/computergraphics/applications/CGFrame.java
+++ src/computergraphics/applications/CGFrame.java
@@ -32,44 +32,10 @@ public class CGFrame extends AbstractCGFrame {
 	 */
 	public CGFrame(int timerInverval) {
 		super(timerInverval);
-		ColorNode colorNode = new ColorNode(new Vector3(0.25, 0.25, 0.75));
-		ColorNode colorNode2 = new ColorNode(new Vector3(0, 1, 0));
-		
-		//
-		ScaleNode scaleNode = new ScaleNode(new Vector3(1, 1, 1));
-		RotationNode rotationNode = new RotationNode(new Vector3(1,0,0), 87);
-		
+		ColorNode colorNode = new ColorNode(new Vector3(0, 0, 0));
 		SingleTriangleNode triangleNode = new SingleTriangleNode();
-		SingleTriangleNode triangleNode2 = new SingleTriangleNode();
-		
-		
-		BetterTriangleNode betterTriangle = new BetterTriangleNode();
-		
-		
-		TranslationNode trans = new TranslationNode(new Vector3(1,1,0));
-		
-//		getRoot().addChild(colorNode);
-//		colorNode.addChild(triangleNode);
-		
-		getRoot().addChild(rotationNode);
-		rotationNode.addChild(trans);
-		
-		
-//		getRoot().addChild(colorNode2);
-//		colorNode2.addChild(triangleNode2);
-		
-//		getRoot().addChild(trans);
-		trans.addChild(colorNode);
-		colorNode.addChild(betterTriangle);
-		
-		
-		//
-//		getRoot().addChild(scaleNode);
-//		scaleNode.addChild(triangleNode);
-//		scaleNode.addChild(colorNode);
-		
-		
-		
+		getRoot().addChild(colorNode);
+		colorNode.addChild(triangleNode);
 	}
 
 	/*
