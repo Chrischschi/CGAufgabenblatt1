6356f5d7c10fbcd77d45e1f57053424711edf5cd
Chrischschi <Christian.Schirin@haw-hamburg.de>
2014-10-07 09:42:58 +0200

    Aufgabe1c Header für aufgabe1c hinzugefügt

--- src/computergraphics/scenegraph/SingleTriangleNode.java
+++ src/computergraphics/scenegraph/SingleTriangleNode.java
@@ -1,3 +1,9 @@
+/**
+ * Praktikum WPCG, WS 14/15, Gruppe 2
+ * Gruppe: Andreas Mauritz (andreas.mauritz@haw-hamburg.de)
+ * 		   Christian Schirin (christian.schirin@haw-hamburg.de)
+ * Aufgabe: Aufgabenblatt 1, Aufgabe 1c
+ */
 package computergraphics.scenegraph;
 
 import javax.media.opengl.GL2;
